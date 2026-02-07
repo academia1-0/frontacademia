47dc1c0194199c8129e7d321dcf38d1952ab0f0d
Johthema <mauriciojodi@gmail.com>
2026-02-07 16:18:46 -0400

    Atualiza versao Header

--- src/pages/equipe/index.jsx
+++ src/pages/equipe/index.jsx
@@ -1,8 +1,19 @@
+import Style from '../../app/globals.css'
 
+
+import HEADER from "../../components/header/index"
 export default function equipes(){
     return(
-        <div>
-            <h1>Cadastro de equipe</h1>
-        </div>
+        
+     
+        <div className="bg-gray-900">
+        <HEADER></HEADER>        
+        
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+        <h1>Cadastro de equipe</h1>
+            </div>
+        
+            </div>
+           
     )
 }
